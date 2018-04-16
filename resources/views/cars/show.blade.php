320226fddf7f6f0bc98360d336fb0cc3e7f4a910
AleksandarSavic95 <alek_bn@hotmail.com>
2018-04-16 15:34:38 +0200

    10-minute MVC tutorial done :)

---
+++ resources/views/cars/show.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Car {{ $car->id }}</title>
+  </head>
+  <body>
+    <h1>Car {{ $car->id }}</h1>
+    <ul>
+      <li>Make: {{ $car->make }}</li>
+      <li>Model: {{ $car->model }}</li>
+      <li>Produced on: {{ $car->produced_on }}</li>
+    </ul>
+  </body>
+</html>
