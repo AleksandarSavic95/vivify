72f112fbd9197d50b619297ac7058045df614005
AleksandarSavic95 <alek_bn@hotmail.com>
2018-04-19 16:09:56 +0200

    #ALSA-8 register user and log him in
    this commit should contain the addition of the "country" field..
    That was added accidentaly in the previous commit so it can't be here :)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,9 +64,12 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'country' => $data['country'],
+            'company' => $data['company'],
         ]);
     }
 }
