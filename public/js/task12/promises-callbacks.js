a42e9b6175b13c62b2bcb3e705725621157bb05b
AleksandarSavic95 <alek_bn@hotmail.com>
2018-04-20 11:48:02 +0200

    #ALSA-12 test promises with callbacks
    Promise is created by invoking the `Promise` c-tor function with `new`.
    We need to call the `resolve` method when promise is fulfilled,
    and the `reject` method when it is rejected.
    After the promise is obtained, we will use it by supplying
    success and failure callbacks to it's `then` method.
    
    Promises can be chained if the success/failure callbacks return promises

--- public/js/task12/promises-callbacks.js
+++ public/js/task12/promises-callbacks.js
@@ -2,3 +2,37 @@
 JavaScript promises and callbacks
 */
 
+var isMomHappy = false;
+
+// Promise
+var willIGetNewPhone = new Promise(
+    function (resolve, reject) {
+        if (isMomHappy) {
+            var phone = {
+                brand: 'iPhone',
+                color: 'gold'
+            };
+            // we need to call the `resolve` method when
+            resolve(phone); // promise is fulfilled
+        } else {
+            var reason = new Error('mom is not happy');
+            // we need to call the `reject` method when
+            reject(reason); // promise is rejected
+        }
+
+    }
+);
+
+// after the promise is obtained, we will use it by supplying
+// success and failure callbacks to it's `then` method
+
+willIGetNewPhone.then(
+    function(phone) {
+        console.log('I got a new phone! It\'s an '
+            + phone.brand + ' of color ' + phone.color);
+    },
+    function(reason) {
+        console.log('Oh no! Mommy won\'t buy me a new phone :(' +
+            ' Here is what happened:\n\t' + reason);
+    }
+);
